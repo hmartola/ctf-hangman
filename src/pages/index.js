946283a59278e18af2db7f002ac4bef868eb556a
hmartola <64286599+hmartola@users.noreply.github.com>
2022-09-05 23:46:31 +0300

    change image source to local

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
+
 import { container } from '../components/css/main.module.css'
+import clue from '../images/ctfAlbinV1.png'
 
 const IndexPage = () => {
   return (
     <div className={container}>
       <main>
-        <img src='https://avatars.githubusercontent.com/u/33056829?s=280&v=4' alt='Albin'></img>
+        <img src={clue} alt='Clue to next step' />
       </main>
     </div>
   )
