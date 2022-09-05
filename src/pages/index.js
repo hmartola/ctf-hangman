7e7725ae04a58ca79938f275f3f42487513398e9
hmartola <64286599+hmartola@users.noreply.github.com>
2022-09-06 00:15:14 +0300

    change to uppercase file extension

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 import { container } from '../components/css/main.module.css'
-import clue from '../images/ctfAlbinV1.png'
+import clue from '../images/ctfAlbinV1.PNG'
 
 const IndexPage = () => {
   return (
